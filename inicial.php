d688c61645bb91c5787d3333665ddaa7aba39046
adilson-jose <64172681+adilson-jose@users.noreply.github.com>
2020-04-22 20:35:07 -0300

    Add files via upload

---
+++ inicial.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+        <link rel= "stylesheet"href ="estilo.css">
+    </head>
+
+    <body>
+        <p>  Natalia de Paiva da Silva </p> 
+    </body>
+</html>
---
+++ processa.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+        <link rel= "stylesheet"href ="CSS/estilo.css">
+    </head>
+
+    <body>
+        
+        <div id="corpo-form">
+        <h1>Entrar</h1>    
+    <form method="POST" action="processa.php" >
+        <input type="email" placeholder="Usuário">
+         <input type="password" placeholder="Senha">
+         <input type="submit" value=" Cadastrar">
+          <a href="cadastrarvaga.php">Ainda não é inscrito?<strong>Cadastre-se</strong>
+    </form>
+          </div>:
+            
+    </body>
+</html>
